b7e51073992db0d40bd3ac3644dca8d9f255068a
Pietro Bertera <pietro@bertera.it>
2015-05-11 12:28:36 +0200

    makedist-w32 now creates a well named binary

--- makedist-w32.bat
+++ makedist-w32.bat
@@ -1,4 +1,10 @@
 set PY_INST=c:\python27_32bit\Scripts\pyinstaller.exe
-%PY_INST% --icon img\SPLiT.exe --onefile --clean --distpat=dist_win32 --console SPLiT.py
 
+rmdir /S /Q dist
+
+%PY_INST% --icon img\SPLiT.ico --onefile --clean --distpat=dist --console SPLiT.py
+
+cd dist
+
+rename SPLiT.exe SPLiT_w32.exe
 pause
